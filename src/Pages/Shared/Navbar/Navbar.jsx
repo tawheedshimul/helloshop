9ac3039173b6cd7292474f7784c7144bfac62fa9
tawheedshimul <tawheedshimul@gmail.com>
2025-01-15 20:42:37 +0600

    upate

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { BiHome } from "react-icons/bi";
-import { BsFillPeopleFill, BsPeople } from "react-icons/bs";
+import { BsFillPeopleFill, BsPeople, BsWhatsapp } from "react-icons/bs";
 import ProductSearchCard from "../Search/Searchwork";
 import Message from "./Message";
 import AddToCartButton from "./AddToCartButton";
 import { LuLogIn } from "react-icons/lu";
 import { GoPeople } from "react-icons/go";
 import { MdAppRegistration } from "react-icons/md";
+import { GrLogin } from "react-icons/gr";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -24,20 +25,23 @@ const Navbar = () => {
     <div className="bg-gray-50 bg-opacity-60 backdrop-blur-lg py-3 ">
       <div className="flex items-center justify-between px-4  relative">
         {/* Search Box */}
-        <div className="text-2xl flex items-center">
-
-          <Link to="/" className="">
-            <BiHome className="text-2xl hover:text-green-600 duration-200" />
-          </Link>
-          <Link to="/" className=" px-3">
-            <GoPeople className="text-2xl hover:text-green-600 duration-200" />
-          </Link>
-
+        <div className="hidden sm:block">
+          <div className="flex items-center justify-between space-x-3">
+            <Link to="/" className="">
+              <BiHome className="text-xl hover:text-green-600 duration-200" />
+            </Link>
+            <Link to="/" className=" px-">
+              <GoPeople className="text-xl hover:text-green-600 duration-200" />
+            </Link>
+            <Link to="/" className=" px-">
+              <BsWhatsapp className="text- hover:text-green-600 duration-200" />
+            </Link>
+          </div>
         </div>
 
         {/* Dropdown Menu */}
         <div
-          className={`absolute left-0 top-10 z-50 w-full bg-white sm:static sm:w-auto sm:flex sm:bg-transparent transition-all duration-300 ease-in-out ${isOpen ? "block" : "hidden"
+          className={`absolute  left-0 top-10 z-50 w-full bg-white sm:static sm:w-auto sm:flex sm:bg-transparent transition-all duration-300 ease-in-out ${isOpen ? "block" : "hidden"
             }`}
         >
           <ul className="flex mt-2 mr-4 flex-col sm:flex-row gap-4 sm:gap-6 text-gray-700 text-right sm:p-0">
@@ -68,13 +72,57 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-
         {/* Social Media Icons or Additional Buttons */}
-        <div className="flex items-center space-x-2">
+        <div className="hidden sm:block">
+          <div className="flex items-center space-x-3 ">
+            <div className="px-">
+              <ProductSearchCard />
+            </div>
+            <GrLogin />
+            {/* <Message /> */}
+            <AddToCartButton />
+            <button
+              className="focus:outline-none sm:hidden"
+              onClick={toggleMenu}
+              aria-label="Toggle Menu"
+            >
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d={
+                    isOpen
+                      ? "M6 18L18 6M6 6l12 12" // X icon when open
+                      : "M4 6h16M4 12h16M4 18h16" // Hamburger icon when closed
+                  }
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
 
-          <div className="px-3">
+        {/* mobile nav  */}
+        <div className="flex items-center justify-between sm:hidden w-full text-xl">
+          <Link to="/" className="">
+            <BiHome className="text-xl hover:text-green-600 duration-200" />
+          </Link>
+          <Link to="/" className=" px-">
+            <GoPeople className="text-xl hover:text-green-600 duration-200" />
+          </Link>
+          <Link to="/" className=" px-">
+            <BsWhatsapp className="text- hover:text-green-600 duration-200" />
+          </Link>
+          <div className="px-">
             <ProductSearchCard />
           </div>
+          <GrLogin />
           {/* <Message /> */}
           <AddToCartButton />
           <button
