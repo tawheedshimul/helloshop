6c9ec5d3a09b6d34a7fb0d3b92ff7cc218741d24
tawheedshimul <tawheedshimul@gmail.com>
2025-01-15 21:30:04 +0600

    update

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -23,25 +23,98 @@ const Navbar = () => {
 
   return (
     <div className="bg-gray-50 bg-opacity-60 backdrop-blur-lg py-3 ">
-      <div className="flex items-center justify-between px-4 text-xl relative">
+      <div className="flex items-center justify- px-4 text-xl relative">
         {/* Search Box */}
-        <div className="hidden sm:block">
-          <div className="flex items-center justify-between space-x-5">
-            <Link to="/" className="">
-              <BiHome className="text-xl hover:text-green-600 duration-200" />
-            </Link>
-            <Link to="/" className=" px-">
-              <GoPeople className="text-xl hover:text-green-600 duration-200" />
-            </Link>
-            <Link to="/" className=" px-">
-              <BsWhatsapp className="text- hover:text-green-600 duration-200" />
-            </Link>
+
+        {/* lg nav  */}
+        <div className="hidden sm:block text-center w-full">
+          <div className="flex items-center justify-center space-x-3">
+            <div className="">
+              <div className="flex items-center justify-between space-x-5">
+                <Link to="/" className="">
+                  <BiHome className="text-xl hover:text-green-600 duration-200" />
+                </Link>
+                <Link to="/" className=" px-">
+                  <GoPeople className="text-xl hover:text-green-600 duration-200" />
+                </Link>
+                <Link to="/" className=" px-">
+                  <BsWhatsapp className="text- hover:text-green-600 duration-200" />
+                </Link>
+              </div>
+            </div>
+
+            {/* Dropdown Menu */}
+            <div
+              className={`absolute  left-0 top-10 z-50 w-full bg-white sm:static sm:w-auto sm:flex sm:bg-transparent transition-all duration-300 ease-in-out ${isOpen ? "block" : "hidden"
+                }`}
+            >
+              <ul className="flex mt-2 mr-4 flex-col sm:flex-row gap-4 sm:gap-6 text-gray-700 text-right sm:p-0">
+                <li>
+                  <Link to="/" className="animate-button">
+                    MEN
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/shipping-agencies" className="animate-button">
+                    WOMEN
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/about" className="animate-button">
+                    KIDS
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/login" className="animate-button">
+                    ELECTRONICS
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/login" className="animate-button">
+                    BUILDINGS
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            {/* Social Media Icons or Additional Buttons */}
+            <div className="flex items-center space-x-5 ">
+              <div className="px-">
+                <ProductSearchCard />
+              </div>
+              <GrLogin />
+              {/* <Message /> */}
+              <AddToCartButton />
+              <button
+                className="focus:outline-none sm:hidden"
+                onClick={toggleMenu}
+                aria-label="Toggle Menu"
+              >
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d={
+                      isOpen
+                        ? "M6 18L18 6M6 6l12 12" // X icon when open
+                        : "M4 6h16M4 12h16M4 18h16" // Hamburger icon when closed
+                    }
+                  ></path>
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
 
-        {/* Dropdown Menu */}
+        {/* mobile nav  */}
         <div
-          className={`absolute  left-0 top-10 z-50 w-full bg-white sm:static sm:w-auto sm:flex sm:bg-transparent transition-all duration-300 ease-in-out ${isOpen ? "block" : "hidden"
+          className={`absolute sm:hidden left-0 top-10 z-50 w-full bg-white sm:static sm:w-auto sm:bg-transparent transition-all duration-300 ease-in-out ${isOpen ? "block" : "hidden"
             }`}
         >
           <ul className="flex mt-2 mr-4 flex-col sm:flex-row gap-4 sm:gap-6 text-gray-700 text-right sm:p-0">
@@ -72,43 +145,6 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-        {/* Social Media Icons or Additional Buttons */}
-        <div className="hidden sm:block">
-          <div className="flex items-center space-x-5 ">
-            <div className="px-">
-              <ProductSearchCard />
-            </div>
-            <GrLogin />
-            {/* <Message /> */}
-            <AddToCartButton />
-            <button
-              className="focus:outline-none sm:hidden"
-              onClick={toggleMenu}
-              aria-label="Toggle Menu"
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d={
-                    isOpen
-                      ? "M6 18L18 6M6 6l12 12" // X icon when open
-                      : "M4 6h16M4 12h16M4 18h16" // Hamburger icon when closed
-                  }
-                ></path>
-              </svg>
-            </button>
-          </div>
-        </div>
-
-        {/* mobile nav  */}
         <div className="flex items-center justify-between sm:hidden w-full text-xl">
           <Link to="/" className="">
             <BiHome className="text-xl hover:text-green-600 duration-200" />
--- src/Pages/Shared/Search/Searchwork.jsx
+++ src/Pages/Shared/Search/Searchwork.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FaSearch, FaChevronDown } from 'react-icons/fa';
 import { FiShoppingCart } from 'react-icons/fi';
+import { IoSearchOutline } from 'react-icons/io5';
 
 const ProductSearchCard = () => {
     const [searchTerm, setSearchTerm] = useState('');
@@ -66,7 +67,8 @@ const ProductSearchCard = () => {
                     onClick={() => setShowModal(true)}
                     className="py-2 rounded-md focus:outline-none  hover:text-green-500"
                 >
-                    <FaSearch className="" />
+                    <IoSearchOutline />
+
                 </button>
             </div>
 
@@ -86,7 +88,8 @@ const ProductSearchCard = () => {
                                     onChange={(e) => setSearchTerm(e.target.value)}
                                     className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                                 />
-                                <FaSearch className="absolute right-3 top-3 text-gray-400" />
+                                <IoSearchOutline className="absolute right-3 top-3 text-gray-400" />
+
                             </div>
                             <div className='flex items-center justify-between'>
                                 <div className="relative">
